69f3dcfca8ea8f5de7c5fb15944ac135eaeb3fba
Kenny Nagano <nagano564@gmail.com>
2018-06-01 19:50:48 -0400

    refactored

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,15 +2,15 @@ class ShowsController < ApplicationController
   require 'uri'
   require 'byebug'
   
-    def index
-      api_key = ENV['MY_API_ENV']  
+  def index
+    api_key = ENV['MY_API_ENV']  
 
-      response = HTTParty.get("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV["MY_API_ENV"]}&sort_by=popularity.desc")
+    response = HTTParty.get("https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&sort_by=popularity.desc")
 
-      @popular_shows = response['results'].map do |movie|
-        Movie.new(movie)
-      end
+    @popular_shows = response['results'].map do |movie|
+      Movie.new(movie)
     end
+  end
 
   def search
   end
