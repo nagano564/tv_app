bf9cdfce9d9eb6ea6e9fe719987b73b29c3093ff
Kenny Nagano <nagano564@gmail.com>
2018-06-01 19:36:53 -0400

    popular shows on homepage working

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,10 +3,11 @@ class ShowsController < ApplicationController
   require 'byebug'
   
     def index
-      api_key = ENV['MY_API_ENV']
-      response = HTTP.get('https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false')
-  
-      @popular_shows = response.parse['results'].map do |movie|
+      api_key = ENV['MY_API_ENV']  
+
+      response = HTTParty.get("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV["MY_API_ENV"]}&sort_by=popularity.desc")
+
+      @popular_shows = response['results'].map do |movie|
         Movie.new(movie)
       end
     end
