7794e783dea7cca366e3910e8f4a2abc64bdb079
Kenny Nagano <nagano564@gmail.com>
2018-06-01 12:42:30 -0400

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'shows#index'
+
+  get 'shows/search'
+
+  get 'shows/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
