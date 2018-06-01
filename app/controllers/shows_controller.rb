644c04f4d468cda8891dcaf2d8ac04f60091dac6
Kenny Nagano <nagano564@gmail.com>
2018-06-01 18:54:53 -0400

    set up index controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,6 +1,15 @@
 class ShowsController < ApplicationController
-  def index
-  end
+  require 'uri'
+  require 'byebug'
+  
+    def index
+      api_key = ENV['MY_API_ENV']
+      response = HTTP.get('https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false')
+  
+      @popular_shows = response.parse['results'].map do |movie|
+        Movie.new(movie)
+      end
+    end
 
   def search
   end
