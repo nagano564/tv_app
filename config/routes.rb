908129db0310936e3815d7b80450c2e23c74af93
Kenny Nagano <nagano564@gmail.com>
2018-06-03 19:23:55 -0400

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do  
 
   root 'shows#index'
+  get 'shows/about'
 
   get 'shows/search', to: 'shows#search'
   resources :shows, only: [:index, :show]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
